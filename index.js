c41f5bc986ca2b92787014e1b3fbecbff29cabed
Kemunto Maragia <kmaragia@email.wm.edu>
2021-02-06 01:12:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const append = ["Milo","Otis","Garfield"].shift("Odie")
 const prepend = ["Milo","Otis","Garfield"].unshift("Odie")
-const removeLast =["Milo","Otis","Garfield"].pop
+const removeLast = ["Milo","Otis","Garfield"]
 const removeFirst =["Milo","Otis","Garfield"]
+
+removeLast.pop
